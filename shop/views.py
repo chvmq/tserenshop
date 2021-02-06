7c20d6be20588e2ba4007ee8d514eed558862a45
chvmq <ttttmmm3264@gmail.com>
2021-02-06 21:22:54 +0300

    Add index, category views

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Notebook, Smartphone
+from .models import Notebook, Smartphone, Category, LatestProducts
 
 
-def test_view(request):
+def index(request):
     print(request)
-    return HttpResponse('hello world')
+    products = LatestProducts.objects.get_products_for_main_page('notebook', 'smartphone')
+    context = {
+        'products': products,
+    }
+    return render(request, 'shop/index.html', context)
 
 
 class ProductDetailView(DetailView):
-
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
         'smartphone': Smartphone,
@@ -21,9 +24,16 @@ class ProductDetailView(DetailView):
         self.queryset = self.model._base_manager.all()
         return super().dispatch(request, *args, **kwargs)
 
-
     # model = Model
     # queryset = Model.objects.all()
     context_object_name = 'product'
     template_name = 'shop/product_detail.html'
+    slug_url_kwarg = 'slug'
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    queryset = Category.objects.all()
+    context_object_name = 'category'
+    template_name = 'shop/category_detail.html'
     slug_url_kwarg = 'slug'
