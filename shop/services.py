d635756bab3168dd154c29ed680ac90014a77bf8
chvmq <ttttmmm3264@gmail.com>
2021-02-16 19:38:58 +0300

    Logic add products to cart

--- shop/services.py
+++ shop/services.py
@@ -1,23 +1,47 @@
 from django.http import HttpResponseRedirect
-from .models import Customer, Cart, CartProduct
+from .models import CartProduct
+from .utils import CartMixin
+from django.views.generic import View
 from django.contrib.contenttypes.models import ContentType
+from django.db.models import Count, Sum
 
 
-def AddProductToCart(request, *args, **kwargs):
+class AddProductToCart(CartMixin, View):
     """Добавляет товар в корзину"""
-    ct_model = kwargs.get('ct_model')
-    product_slug = kwargs.get('slug')
-    customer = Customer.objects.get(user=request.user)
-    cart = Cart.objects.get(owner=customer, in_order=False)
-    content_type = ContentType.objects.get(model=ct_model)
-    product = content_type.model_class().objects.get(slug=product_slug)
-    cart_product, created = CartProduct.objects.get_or_create(
-        user=cart.owner,
-        cart=cart,
-        content_type=content_type,
-        object_id=product.id,
-    )
-    if created:
-        cart.products.add(cart_product)
 
-    return HttpResponseRedirect('/cart/')
+    def get(self, *args, **kwargs):
+        ct_model = kwargs.get('ct_model')
+        product_slug = kwargs.get('slug')
+
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=self.cart.owner,
+            cart=self.cart,
+            content_type=content_type,
+            object_id=product.id,
+        )
+
+        cart_product.final_price = cart_product.number * cart_product.content_object.price
+
+        if created:
+            self.cart.products.add(cart_product)
+        if not self.cart.products:
+            self.cart.total_products = 0
+            self.cart.final_price = 0
+
+        cart_data = self.cart.products.aggregate(Sum('final_price'), Count('id'))
+        if cart_data.get('final_price__sum'):
+            self.cart.final_price = cart_data['final_price__sum']
+        else:
+            self.cart.final_price = 0
+
+        if cart_data.get('id__count'):
+            self.cart.total_products = cart_data['id__count']
+        else:
+            self.cart.total_products = cart_data['id__count']
+
+        cart_product.save()
+        self.cart.save()
+        return HttpResponseRedirect('/cart/')
