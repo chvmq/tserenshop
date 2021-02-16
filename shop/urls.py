691a6987f0a3a36481af60f3ea81c4bb0c2f08de
chvmq <ttttmmm3264@gmail.com>
2021-02-16 19:39:32 +0300

    Logic add product to cart

--- shop/urls.py
+++ shop/urls.py
@@ -3,10 +3,10 @@ from .views import *
 from .services import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('product/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('category/<str:slug>/', CategoryListView.as_view(), name='category_list'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('add_to_cart/<str:ct_model>/<str:slug>/', AddProductToCart, name='add_to_cart'),
+    path('add_to_cart/<str:ct_model>/<str:slug>/', AddProductToCart.as_view(), name='add_to_cart'),
 
 ]
