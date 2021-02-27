0f04aa52e0a7c572088049c5f26e8f34039424ed
chvmq <ttttmmm3264@gmail.com>
2021-02-28 00:23:24 +0300

    Updated static

--- tserenshop/settings.py
+++ tserenshop/settings.py
@@ -78,10 +78,10 @@ DATABASES = {
     }
 }
 
-# if os.getlogin() != 'lari':
-#     import dj_database_url
-#     db_from_env = dj_database_url.config()
-#     DATABASES['default'].update(db_from_env)
+if os.getlogin() != 'lari':
+    import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -124,7 +124,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
