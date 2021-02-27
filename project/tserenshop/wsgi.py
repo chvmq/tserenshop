2e3db0837a8ec8b4845b70a62d03468d0be9b38e
chvmq <ttttmmm3264@gmail.com>
2021-02-27 23:55:59 +0300

    Removed

--- project/tserenshop/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-from whitenoise.django import DjangoWhiteNoise
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tserenshop.settings')
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
